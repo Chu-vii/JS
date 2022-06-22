161e2ef1a038704f37d0ed0434e2524273670097
Chu-vii <yoharuu@gmail.com>
2022-06-22 17:10:08 +0300

    add HW_3.3.js

---
+++ HW_3.3(extra).js
@@ -0,0 +1,189 @@
+//HW_3
+
+// Task 1. Написать функцию, которая найдет и выведет в консоль юзеров, зарегистрированных 09.10.2021 и 10.10.2021). Массив в task1.txt
+
+const users = [
+    {
+        firstName: 'John',
+        lastName: 'Caper',
+        phone: '7436676737634',
+        registrationDate: '12.10.2021'
+    },
+    {
+        firstName: 'Clark',
+        lastName: 'Kent',
+        phone: '4346676737634',
+        registrationDate: '16.09.2021'
+    },
+    {
+        firstName: 'Tony',
+        lastName: 'Stark',
+        phone: '7436698337634',
+        registrationDate: '11.10.2021'
+    },
+    {
+        firstName: 'Bruce',
+        lastName: 'Wayne',
+        phone: '1111176737634',
+        registrationDate: '09.10.2021'
+    },
+    {
+        firstName: 'Star',
+        lastName: 'Lord',
+        phone: '7439374737634',
+        registrationDate: '10.10.2021'
+    },
+    {
+        firstName: 'Kate',
+        lastName: 'Bishop',
+        phone: '7436693647634',
+        registrationDate: '11.10.2021'
+    },
+    {
+        firstName: 'Jerry',
+        lastName: 'James',
+        phone: '7409048737634',
+        registrationDate: '10.10.2021'
+    },
+    {
+        firstName: 'Jeremy',
+        lastName: 'Clarkson',
+        phone: '743667600289334',
+        registrationDate: '16.10.2020'
+    },
+    {
+        firstName: 'Robert',
+        lastName: 'Patrik',
+        phone: '7436676730093',
+        registrationDate: '10.10.2020'
+    },
+    {
+        firstName: 'Jonny',
+        lastName: 'Sins',
+        phone: '74923982737634',
+        registrationDate: '01.01.2021'
+    },
+    {
+        firstName: 'Andrew',
+        lastName: 'Garfield',
+        phone: '743667988344',
+        registrationDate: '09.10.2019'
+    },
+    {
+        firstName: 'Jane',
+        lastName: 'Foster',
+        phone: '74368783427634',
+        registrationDate: '09.10.2019'
+    },
+    {
+        firstName: 'Rick',
+        lastName: 'Smith',
+        phone: '700000037634',
+        registrationDate: '12.10.2021'
+    },
+];
+
+var newUsers =  users.filter(function(user) {
+    return  user.registrationDate == '10.10.2021' || user.registrationDate == '09.10.2021';
+});
+console.log(newUsers) ;
+
+/******result*****
+[
+    {
+      firstName: 'Bruce',
+      lastName: 'Wayne',
+      phone: '1111176737634',
+      registrationDate: '09.10.2021'
+    },
+    {
+      firstName: 'Star',
+      lastName: 'Lord',
+      phone: '7439374737634',
+      registrationDate: '10.10.2021'
+    },
+    {
+      firstName: 'Jerry',
+      lastName: 'James',
+      phone: '7409048737634',
+      registrationDate: '10.10.2021'
+    }
+  ]
+
+******result*****
+*/
+
+//Task 2* Откройте в VSCode task2.json файл. Скопируйте из него JSONку, вставьте в свой код (присвоив в переменную).
+//Дан массив объектов. Каждый объект является идентификационной карточкой человека. Нам нужно хранить только уникальные значения в этом массиве. Реализуйте функцию, которая будет выполнять эту работу.
+//Task 2*** Реализуйте считывание из JSONки из файла task2.json с помощью, например, модуля fs. для дальнейшего использования в функции, описанной в задании
+
+const customers = require("./task2 (HW3).json"); 
+// не бажано. Если очень большой JSON файл, он захлебнется в цикле событий.
+// require будетможет прочитать файл только один раз. Последующие вызовы, require для того же файла, вернут кэшированную копию. Не подойдет к файлу который постоянно обновляется.
+// Если файл не имеет расширения .json, require не будет обрабатывать содержимое файла как JSON
+
+var fs = require('fs');
+var json = JSON.parse(fs.readFileSync("./task2 (HW3).json", 'utf8'));
+//Синхронная версия
+
+/*var fs = require('fs');
+
+fs.readFile("./task2 (HW3).json", 'utf8', function (err, data) {
+    if (err) throw err; // we'll not consider error handling for now
+    var obj = JSON.parse(data);
+});
+//Асинхронная версия
+*/
+
+
+const removeDuplicates = function(arr) {
+    let persons = [];
+
+    arr.forEach((el) => {
+      if (!persons.includes(JSON.stringify(el))) {
+        persons.push(JSON.stringify(el));
+      }
+    });
+    return persons.map(el => JSON.parse(el));
+}
+console.log('===================BEST========================');
+console.log(removeDuplicates(json).length);
+
+//____________________________________________________________________________
+
+function removeDuplicates1(arrayIn) {
+    let arrayOut =[arrayIn[0]];
+    let arrname = [];
+   //  console.log(arrayIn[0].name);
+    for (let a = 1; a < arrayIn.length; a++) {
+   //     console.log(arrayIn[a].name);
+        arrname.length=0;
+      for (let b = 0; b < arrayOut.length; b++) {
+    //      console.log(arrayOut[b].name);
+          arrname.push(arrayIn[b].name)
+      }
+          if (!arrname.includes(arrayIn[a].name)) {
+          arrayOut.push(arrayIn[a]) 
+        }
+      
+    }
+    arrayIn = arrayOut;
+    return arrayIn;
+  }
+  console.log(removeDuplicates1(customers).length);
+
+
+
+/*Task 3**
+В файле task3.txt найдете структуру компании и задания, необходимые выполнить.
+
+Примечание: ВСЕ задания выполнять не обязательно, если вам люто сложно. Но ПЕРВОЕ - прям надо всем:)
+
+Task 4****
+
+В файле task4.txt вы найдете разноуровневый массив объектов. Очень похожий на массив из 3го задания, только количество вложенностей может быть не ограничено. 
+
+Задание: написать функцию: 
+
+Функция строит древовидный список компании.
+При ее вызове в консоль должен вывестись список подразделений компании с указанием количества сотрудников и с соблюдение вложенности подразделений.*/
